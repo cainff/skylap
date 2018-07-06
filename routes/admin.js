4c0c18afe44fec99059c43061f523b6cd4e2fb45
Florian Saumurot <fsaumurot@skillbase.fr>
2018-07-06 02:29:31 +0200

    admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express'),
+    fs = require('fs'),
+    Papa = require('papaparse'),
+    router = express.Router();
+
+const user = {
+    mail: 'toto@gmail.com',
+    password: 'totolola42'
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function (req, res, next) {
+    res.render('connexion')
 });
+
+router.post('/login', (req, res, next) => {
+    if (user.mail === req.body.mail && user.password === req.body.password)
+        res.redirect('/admin/dashboard')
+    else
+        res.redirect('/admin/login')
+})
+
+router.get('/dashboard', (req, res, next) => {
+    const file = fs.createReadStream('public/csv/data.csv')
+    Papa.parse(file, {
+        header: true,
+        download: true,
+        dynamicTyping: true,
+        complete: (results) => {
+            res.render('admin', {
+                data: results.data
+            })
+        }
+    })
+})
 
 module.exports = router;
